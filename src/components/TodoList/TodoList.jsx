5e5529e6eff7bf79b21a7cf8a30cb4e0cdf4e25e
Atefeh-MN <atefeh.nasrabadi1993@gmail.com>
2021-09-30 19:36:08 +0330

    add todolist and todo

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,8 +1,21 @@
 
-
-const TodoList = () => {
+import {BsTrash,BsCheck } from "react-icons/bs";
+import style from './todolist.module.css';
+const TodoList = (props) => {
+    const todolist=[...props.todos]
     return ( 
-        <div>Todolist</div>
+        <div>
+            <p>Todo List</p>
+            <ul>
+                {todolist.map((p)=>{return(
+                <li key={p.id}>
+                  <p className={style.todo}>{p.text}</p>
+                  <button className={style.button} ><BsCheck/></button>
+                  <button  className={style.button}><BsTrash/></button>
+                </li>)})}
+            </ul>
+
+        </div>
      );
 }
  
